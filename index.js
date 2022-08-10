ae1d56ba7a8855a2a035ada8c4662452df293316
madelinedulchin <madeline.dulchin@gmail.com>
2022-08-09 20:47:28 -0400

    commit tree 3d1130fb115743d27a4873f80ce1d3c13dc61c36

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    n++
+    return (n)
+}
+increment(12)
+function decrement(n){
+    n--
+    return (n)
+}
+decrement(15)
+
+function makeInt(n){
+    return parseInt(n, 10)
+    
+}
+function preserveDecimal(n){
+return parseFloat(n, 10)
+}
